5ae7427b54bcc57181bb1e272fefab2e37e1948d
Samuel Costanzo <costanzodev25@gmail.com>
2026-06-16 09:37:24 -0400

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center px-10 py-5 border-b border-black/10 bg-[#f4f1ec]">
+      <h1>Sam Costanzo</h1>
+    </nav>
+  );
+}
