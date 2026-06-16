44731d499e510b76f7ab3f0f95f3b4a31339104d
Sam Costanzo <samcostanzo@Sams-MacBook-Air.local>
2026-06-16 10:46:40 -0400

    Worked in kitchen on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,18 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-10 py-5 border-b border-black/10 bg-[#f4f1ec]">
       <h1>Sam Costanzo</h1>
+      <div className="flex justify-between items-center gap-5">
+        <div className="menu">
+          <ul className="flex gap-5">
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="badge border border-accent px-5 py-1">
+          Open to work
+        </div>
+      </div>
     </nav>
   );
 }
